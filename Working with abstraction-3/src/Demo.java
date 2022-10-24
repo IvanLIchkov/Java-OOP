0a1ec8d8b02866fce380659d4c6551c7a0278c69
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-24 10:47:43 +0300

    Enumeration demo.

--- Working with abstraction-3/src/Demo.java
+++ Working with abstraction-3/src/Demo.java
@@ -2,5 +2,10 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);}
+        Scanner scan = new Scanner(System.in);
+
+        DayOfWeek dayOfWeek=DayOfWeek.TUESDAY;
+
+        System.out.println(dayOfWeek.getDayNumber());
+    }
 }
