22f9fe53c714ee74dfdd73e4bf928d14b7a45349
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-25 15:00:47 +0300

    Immutable team class added in task.

--- Encapsulation-5/src/SortByNameAndAge/Main.java
+++ Encapsulation-5/src/SortByNameAndAge/Main.java
@@ -14,20 +14,27 @@ public class Main {
 
         for (int i = 0; i < numberOfPeople; i++) {
             String[] input = scan.nextLine().split(" ");
-            String firstName=input[0];
-            String lastName=input[1];
-            int age=Integer.parseInt(input[2]);
-            double salary =Double.parseDouble(input[3]);
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
 
-            Person personToAdd=new Person(firstName,lastName,age, salary);
+            Person personToAdd = new Person(firstName, lastName, age, salary);
             people.add(personToAdd);
         }
-        double bonus=Double.parseDouble(scan.nextLine());
-
+//        double bonus=Double.parseDouble(scan.nextLine());
+        Team team = new Team("Black Eagles");
         for (Person person : people) {
-            person.increaseSalary(bonus);
-            System.out.println(person.toString());
+            team.addPlayer(person);
         }
+//        for (Person person : people) {  TODO: the list are immutable in getList method in class person, that's why they can't be changed.
+//            if (person.getAge() < 40) {
+//                team.getFirstTeam().add(person);
+//            } else {
+//                team.getReservedTeam().add(person);
+//            }
+//        }
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReservedTeam().size() + " players");
     }
-
 }
