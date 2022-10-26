9db53d82abe6a1c53de8ced7926cc4577f543101
Scopi1 <Vanko.lichkov@abv.bg>
2022-10-26 15:40:31 +0300

    Shopping spree task.

--- EncapsulationExercise-6/src/ShoppingSpree/Main.java
+++ EncapsulationExercise-6/src/ShoppingSpree/Main.java
@@ -1,4 +1,69 @@
 package ShoppingSpree;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<Person> people = new ArrayList<>();
+        String[] peopleInfo = scan.nextLine().split(";");
+        for (String person : peopleInfo) {
+            String[] tokens = person.split("=");
+            String name = tokens[0];
+            double money = Double.parseDouble(tokens[1]);
+            try{
+                Person personToAdd = new Person(name, money);
+                people.add(personToAdd);
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                return;
+            }
+
+        }
+        List<Product> products = new ArrayList<>();
+        String[] productInfo = scan.nextLine().split(";");
+        for (String product : productInfo) {
+            String[] tokens = product.split("=");
+            String name = tokens[0];
+            double cost = Double.parseDouble(tokens[1]);
+            try {
+                Product productToAdd = new Product(name, cost);
+                products.add(productToAdd);
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                return;
+            }
+        }
+        String command = scan.nextLine();
+        while (!command.equals("END")) {
+            String[] tokens = command.split("\\s+");
+            String personName = tokens[0];
+            String productName = tokens[1];
+            Product product = products.stream()
+                    .filter(p -> p.getName()
+                            .equals(productName))
+                    .findFirst()
+                    .orElse(null);
+            people.stream()
+                    .filter(p -> p.getName()
+                            .equals(personName))
+                    .forEach(p -> p.buyProduct(product));
+            command = scan.nextLine();
+        }
+        people.stream().forEach(p -> {
+            System.out.printf("%s - ", p.getName());
+            if (p.getProducts().size() > 0) {
+                List<String> productNames = new ArrayList<>();
+                p.getProducts().forEach(product -> productNames.add(product.getName()));
+                System.out.println(String.join(", ", productNames));
+            } else {
+                System.out.println("Nothing bought");
+            }
+        });
+    }
 }
--- EncapsulationExercise-6/src/ShoppingSpree/Person.java
+++ EncapsulationExercise-6/src/ShoppingSpree/Person.java
@@ -1,4 +1,69 @@
 package ShoppingSpree;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Person {
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        setName(name);
+        setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setMoney(double money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
+
+    public void buyProduct(Product product) {
+        if (product==null){
+            throw new IllegalArgumentException();
+        }
+        if (product.getCost() <= money) {
+            products.add(product);
+            money=(money - product.getCost());
+            System.out.printf("%s bought %s%n", name, product.getName());
+        } else {
+            System.out.printf("%s can't afford %s%n", name, product.getName());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        if (this.products.isEmpty()) {
+            return this.name + " - Nothing bought";
+        }
+        return this.name +
+                " - " +
+                this.products
+                        .stream()
+                        .map(Product::toString)
+                        .collect(Collectors.joining(", "));
+    }
 }
