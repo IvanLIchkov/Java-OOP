0bacb5313dc60ce6e1e8b1dabbeb11e84719962f
Scopi1 <Vanko.lichkov@abv.bg>
2022-11-05 20:29:24 +0200

    Reflection getters and setters types parameters update.

--- ReflectionAndAnotation-15/src/Main.java
+++ ReflectionAndAnotation-15/src/Main.java
@@ -2,6 +2,7 @@ import java.lang.reflect.*;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.TreeSet;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -13,11 +14,13 @@ public class Main {
         TreeSet<Method> getters = filterMethodsBy(declaredMethods,"get");
         TreeSet<Method> setters = filterMethodsBy(declaredMethods,"set");
 
+        Function<Class<?>, String>formatType=c->c==int.class?"class int": c.toString();
+
         getters.forEach(m->{
-            System.out.printf("%s will return class %s%n",m.getName(),m.getReturnType());
+            System.out.printf("%s will return %s%n",m.getName(),formatType.apply(m.getReturnType()));
         });
         setters.forEach(m->{
-            System.out.printf("%s and will set field of class %s%n",m.getName(),m.getParameterTypes()[0]);
+            System.out.printf("%s and will set field of %s%n",m.getName(),formatType.apply(m.getParameterTypes()[0]));
         });
     }
     public static TreeSet<Method> filterMethodsBy(Method[] methods,String filter){
