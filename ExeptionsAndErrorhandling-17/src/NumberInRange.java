03e3176f4fb45ce727251ff2db121171438a050a
Scopi1 <Vanko.lichkov@abv.bg>
2022-11-10 10:46:12 +0200

    Marto lector solution.

--- ExeptionsAndErrorhandling-17/src/NumberInRange.java
+++ ExeptionsAndErrorhandling-17/src/NumberInRange.java
@@ -1,39 +1,37 @@
+import javax.swing.text.html.Option;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class NumberInRange {
-    private static Scanner scan = new Scanner(System.in);
-    private  final static int a= scan.nextInt();
-    private  final static int b= scan.nextInt();
     public static void main(String[] args) {
-        scan.nextLine();
-        System.out.printf("Range: [%d...%d]%n",a,b);
+        Scanner scan = new Scanner(System.in);
 
+        int[] range = Arrays.stream(scan.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int begin=range[0];
+        int end=range[1];
 
-        while (!validateNumber()){
-            validateNumber();
-        }
-    }
-    public static boolean validateNumber(){
-        try {
-            System.out.printf("Valid number: %d",readNumber());
-            return true;
-        }catch (NumberFormatException e){
-            System.out.println(e.getMessage());
-        }
-        return false;
-    }
+        String input= scan.nextLine();
 
-    public static int readNumber(){
-        String n= scan.nextLine();
-        try {
-            int number=Integer.parseInt(n);
-            if (a <= number && b >= number) {
-                return number;
-            } else {
-                throw new NumberFormatException("Invalid number: " + n);
+        boolean isInRange = false;
+        System.out.printf("Range: [%d...%d]%n",begin,end);
+
+        while (!isInRange){
+            Optional<Integer> number = Optional.empty();
+            try{
+                number =Optional.of(Integer.parseInt(input));
+            }catch (NumberFormatException ignored){
             }
-        }catch (NumberFormatException e){
-            throw new NumberFormatException("Invalid number: " + n);
+
+            if (number.isEmpty() || number.get()<begin || number.get()>end){
+                System.out.println("Invalid number: "+input);
+                input = scan.nextLine();
+            }else{
+                isInRange = true;
+            }
         }
+        System.out.println("Valid number: "+ input);
     }
 }
